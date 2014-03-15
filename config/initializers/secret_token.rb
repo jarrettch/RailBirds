5d18660ddb5e443b7941e449defd66f200dff62a
jarrettch <jarrett.harris@gmail.com>
2014-03-15 13:39:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailBirds::Application.config.secret_key_base = 'f18a436f2d96ec5f553407ad78006dad8029bcf45eec54e9fb4063d20ed2ac14d4f60193642b9c32b50cc9fd2ec5b89855ff5f8ba0d533c718b57ae8c1ed13b6'
