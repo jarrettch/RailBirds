08cc8bb719b32a6eeb9112afc9f884b240c49398
jarrettch <jarrett.harris@gmail.com>
2014-03-17 01:43:18 -0700

    add devise views, foundation 5, navbar

--- app/controllers/cash_games_controller.rb
+++ app/controllers/cash_games_controller.rb
@@ -1,2 +1,5 @@
 class CashGamesController < ApplicationController
+
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ RailBirds::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cash_games#index'
+
+  resources :cashgames
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
