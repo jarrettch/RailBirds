9a513dc2b3b6f13149056ea9c7a4a65274662377
jarrettch <jarrett.harris@gmail.com>
2014-03-25 17:41:05 -0700

    added navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+//= require icheck.min
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
